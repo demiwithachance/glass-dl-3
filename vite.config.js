5db21d07b49a69fd0588a2e17806599a53360b1f
demiwithachance <d3m1g0dent@gmail.com>
2026-06-14 09:06:10 -0400

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   clearScreen: false,
   publicDir: "assets",
+  build: {
+    target: "esnext",
+  },
   server: {
     port: 1420,
     strictPort: true,
